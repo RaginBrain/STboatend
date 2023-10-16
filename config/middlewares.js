a925c8a305d8caa4c8697efd00465a98d43ac5e3
Antimon <ante@alterego.hr>
2023-10-16 09:48:16 +0200

    middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = {
+  load: {
+    before: ['responseTime', 'logger', 'cors', 'responses', 'gzip'],
+    order: [
+      "Define the middlewares' load order by putting their name in this array in the right order",
+    ],
+    after: ['parser', 'router'],
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['*'], // For development and testing. Replace with specific domains in production.
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'X-Frame-Options',
+        'Origin',
+        'X-Frame-Options',
+      ],
+    },
+  },
+};
