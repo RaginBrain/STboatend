32a6f993278ebc87d29257fb6a77d1019b798957
Antimon <ante@alterego.hr>
2023-10-28 21:10:04 +0200

    added limit to resolver

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ module.exports = {
     const extension = ({ nexus }) => ({
       typeDefs: `
         extend type Query {
-          getTripps(date: String, location: String, offset: Int! = 0): [Tripp]
+          getTripps(date: String, location: String, offset: Int! = 0, limit: Int! = 15): [Tripp]
         }
       `,
       resolvers: {
         Query: {
           getTripps: async (parent, args) => {
-            const { date, location, offset } = args;
+            const { date, location, offset, limit } = args;
             let filter = {};
 
             if (date) {
@@ -20,9 +20,11 @@ module.exports = {
             if (location) {
               filter.location = location;
             }
-            
-            // Incorporating offset for pagination
+
+            // Incorporating offset and limit for pagination
             filter._start = offset;
+            filter._limit = limit;
+
             return await strapi.services.tripp.find(filter);
           }
         }
