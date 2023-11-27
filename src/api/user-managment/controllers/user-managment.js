efe42e3ccb3a786a942bc3546b8ce6aafbaa5c0d
Antimon <ante@alterego.hr>
2023-11-27 19:24:46 +0100

    Dovrsen mail template

--- src/api/user-managment/controllers/user-managment.js
+++ src/api/user-managment/controllers/user-managment.js
@@ -58,27 +58,29 @@ module.exports = createCoreController('api::user-managment.user-managment', ({st
 
   
       const msg = {
-        to: bookerMail,
+        to: [bookerMail, tourProviderMail],
         from: 'pecati-papiri@yandex.com',
         subject: `Rezervacija ${bookerName}, ${tourName}, ${date}, ${startTime}`,
         html: `Poštovani,<br><br>` +
-            `Vaša rezervacija je uspješno zaprimljena.<br><br>` +
-            `Detalji Vaše rezervacije:<br><br>` +
-            `--------------------------------------<br>` +
-            `- Rezervaciju napravio: ${bookerName}<br>` +
-            `- Naziv ture: <strong>${tourName}</strong><br>` +
-            `- Datum (Y-M-D): <strong>${date}</strong><br>` +
-            `- Vrijeme polaska: <strong>${startTime}</strong><br>` +
-            `- Trajanje ture: ${duration}<br>` +
-            `- Mjesto polaska: ${startLocation}<br>` +
-            `- Brodar: ${tourProvider}<br>` +
-            `- Broj putnika: <strong>${passengerCount}</strong><br>` +
-            `- Ukupna cijena: <strong>${totalPrice}€</strong><br><br>` +
-            `Napomena: ${note}<br>` +
-            `--------------------------------------<br><br>` +
-            `Zahvaljujemo na Vašoj rezervaciji.<br><br>` +
-            `Srdačan pozdrav,<br>`+
-            `STboats` 
+          `Vaša rezervacija je uspješno zaprimljena.<br><br>` +
+          `Detalji Vaše rezervacije:<br><br>` +
+
+          `<div style="font-size: 1.2em; border-top: 2px dotted; border-bottom: 2px dotted;">` + // Increase font size and add dotted border
+          `- Rezervaciju napravio: ${bookerName}<br>` +
+          `- Naziv : <strong>${tourName}</strong><br>` +
+          `- Datum : <strong>${date}</strong><br>` +
+          `- Vrijeme polaska: <strong>${startTime}</strong><br>` +
+          `- Trajanje : ${duration}<br>` +
+          `- Mjesto polaska: ${startLocation}<br>` +
+          `- Brodar: ${tourProvider}<br>` +
+          `- Broj putnika: <strong>${passengerCount}</strong><br>` +
+          `- Ukupna cijena: <strong>${totalPrice}€</strong><br><br>` +
+          `Napomena bookera: ${note}<br>` +
+          `</div> <br>` + // Close the div with increased font size
+
+          `Zahvaljujemo na Vašoj rezervaciji.<br><br>` +
+          `Srdačan pozdrav,<br>` +
+          `STboats`
       };
   
       try {
@@ -88,20 +90,8 @@ module.exports = createCoreController('api::user-managment.user-managment', ({st
         ctx.send({ error: 'Failed to send email.', details: error });
       }
 
-      const msg2 = {
-        to: tourProviderMail,
-        from: 'pecati-papiri@yandex.com',
-        subject: 'Test Email from Strapi',
-        text: 'This is a test email sent from Strapi using SendGrid.',
-        // Optionally include HTML content, templates, etc.
-      };
-  
-      try {
-        await sendgrid.send(msg2);
-        ctx.send({ message: 'Email sent successfully!' });
-      } catch (error) {
-        ctx.send({ error: 'Failed to send email.', details: error });
-      }
+      
+      
     }
 
 })
